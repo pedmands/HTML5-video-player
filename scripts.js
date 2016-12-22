0763ee864a08ad01959796a04c8a436d8114e202
Preston Edmands <pedmands@gmail.com>
2016-12-22 15:48:28 -0500

    Skip buttons and volume/speed sliders hooked up

--- scripts.js
+++ scripts.js
@@ -4,8 +4,10 @@ const video = player.querySelector('.viewer')
 const progress = player.querySelector('.progress')
 const progressBar = player.querySelector('.progress__filled')
 const toggle = player.querySelector('.toggle')
-const skipButtons = player.querySelector('[data-skip]')
-const ranges = player.querySelector('.player__slider')
+const skipButtons = player.querySelectorAll('[data-skip]')
+const ranges = player.querySelectorAll('.player__slider')
+
+let isUpdating = false
 
 // Build our functions
 function togglePlay() {
@@ -17,10 +19,20 @@ function togglePlay() {
 }
 
 function updateButton() {
-	const icon = this.pause ? '►' : '❚ ❚'
+	const icon = this.paused ? '►' : '❚ ❚'
 	toggle.textContent = icon
 }
 
+function skip() {
+	console.log(this.dataset.skip)
+	video.currentTime += parseFloat(this.dataset.skip)
+}
+
+function handleRangeUpdate() {
+	if(!isUpdating) return // flag to update range only when mouse is down.
+
+	video[this.name] = this.value
+}
 
 // Hook up event listeners
 video.addEventListener('click', togglePlay)
@@ -28,3 +40,11 @@ video.addEventListener('play', updateButton)
 video.addEventListener('pause', updateButton)
 
 toggle.addEventListener('click', togglePlay)
+
+skipButtons.forEach(button => button.addEventListener('click', skip))
+
+ranges.forEach(range => range.addEventListener('change', handleRangeUpdate))
+ranges.forEach(range => range.addEventListener('mousemove', handleRangeUpdate))
+ranges.forEach(range => range.addEventListener('mouseup', () => isUpdating = false))
+ranges.forEach(range => range.addEventListener('mousedown', () => isUpdating = true))
+
